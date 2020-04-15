d08bc15e38fcb21a4359cd438df1cfd4eab88978
FlyPythons <schooiboy@qq.com>
2020-04-15 18:29:51 +0800

    support for py2.x

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import os
+import sys
 from setuptools import setup, find_packages
+
+PY_VERSION = 2
+if sys.version[0] == "3":
+    PY_VERSION = 3
 
 
 def get_version():
     """Get version and version_info from markdown/__meta__.py file."""
     module_path = os.path.join(os.path.dirname('__file__'), 'hwget', 'version.py')
 
-    import importlib.util
-    spec = importlib.util.spec_from_file_location('version', module_path)
-    meta = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(meta)
-    return meta.__version__
+    if PY_VERSION == 2:
+        import importlib
+        meta = importlib.import_module('hwget.version')
+        return meta.__version__
+    elif PY_VERSION == 3:
+        import importlib.util
+        spec = importlib.util.spec_from_file_location('version', module_path)
+        meta = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(meta)
+        return meta.__version__
+    else:
+        raise Exception()
 
-
+print(get_version())
+"""
 setup(
     name="Hwget",
     author="Junpeng Fan",
@@ -23,3 +36,4 @@ setup(
     description='Download data with HuaWei cloud',
     url="https://github.com/FlyPythons/hwget",
 )
+"""
